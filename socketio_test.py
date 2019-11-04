36f1ff7e70522fd158aa3a8fd6d25d7b959958b2
manuel <manuel.henn@me.com>
2019-11-04 23:15:52 +0100

    sockets as desired ;)

---
+++ socketio_test.py
@@ -0,0 +1,21 @@
+from aiohttp import web
+import socketio
+
+sio = socketio.AsyncServer()
+app = web.Application()
+
+sio.attach(app)
+
+async def index(request):
+    with open("socketio_client_test.html") as f:
+        return web.Response(text=f.read(), content_type="text/html")
+
+@sio.on("message")
+async def print_message(sid,message):
+    print("Socket ID:", sid)
+    print("Nachricht", message)
+
+app.router.add_get("/", index)
+
+if __name__ == "__main__":
+    web.run_app(app)
